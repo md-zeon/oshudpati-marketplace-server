e30068f05d2dad33f4e1e56b04cc8ed0dae0f62f
md-zeon <zeon.cse@gmail.com>
2026-05-24 12:41:38 +0600

    Better auth set up complete

---
+++ prisma/migrations/20260524063524_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Demo" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Demo_pkey" PRIMARY KEY ("id")
+);
