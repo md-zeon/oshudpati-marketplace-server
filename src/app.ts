e0e43776e5efdf35790ea2f199ae31bafac044c6
md-zeon <zeon.cse@gmail.com>
2026-05-24 13:52:30 +0600

    Initialized Express App

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application } from "express";
+import cors from "cors";
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+export default app;
