9e4772b426b9bfb446b27da55d4d571675da6fbd
md-zeon <zeon.cse@gmail.com>
2026-05-24 18:20:32 +0600

    Add category routes and enhance global error handler for Zod and Prisma Releted Errors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import { AuthRoutes } from "./modules/auth/auth.routes";
 import errorHandler from "./middlewares/globalErrorHandler";
+import { CategoryRoutes } from "./modules/category/category.routes";
 const app: Application = express();
 
 app.use(
@@ -16,8 +17,10 @@ app.use(express.json());
 
 // Auth routes
 app.use("/api/auth", AuthRoutes);
-
 app.all("/api/auth/*splat", toNodeHandler(auth));
+
+// Category routes
+app.use("/api/categories", CategoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { Prisma } from "../../generated/prisma/client";
+import { ZodError } from "zod";
 
 function errorHandler(
   err: unknown,
@@ -10,23 +11,77 @@ function errorHandler(
   let success = false;
   let statusCode = 500;
   let message = "Internal Server Error";
-  let errorDetails: any = null;
+  let errors: any = null;
 
-  if (err instanceof Error) {
+  // Handle Zod validation errors
+  if (err instanceof ZodError) {
+    const formattedErrors = err.issues.map((issue) => ({
+      path: issue.path[issue.path.length - 1],
+      message: issue.message,
+    }));
+
+    statusCode = 400;
+    message = "Validation Error";
+    errors = formattedErrors;
+  }
+
+  // Handle PrismaClientKnownRequestError
+  else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    statusCode = 400;
+    switch (err.code) {
+      case "P2002":
+        statusCode = 409;
+        message = (
+          err.meta?.driverAdapterError as
+            | { cause?: { constraint?: { fields?: string[] } } }
+            | undefined
+        )?.cause?.constraint?.fields
+          ? `Unique constraint failed on the field(s): ${(err.meta?.driverAdapterError as { cause?: { constraint?: { fields?: string[] } } } | undefined)!.cause!.constraint!.fields!.join(", ")}.`
+          : "Unique constraint failed.";
+        errors = err.meta?.driverAdapterError
+          ? [err.meta?.driverAdapterError]
+          : null;
+        break;
+
+      case "P2021":
+        statusCode = 404;
+        message = `The table ${err.meta?.modelName} does not exist in the current database.`;
+        errors = err.meta?.driverAdapterError
+          ? [err.meta?.driverAdapterError]
+          : null;
+        break;
+
+      case "P2025":
+        statusCode = 404;
+        message = "Record not found.";
+        errors = err.meta?.driverAdapterError
+          ? [err.meta?.driverAdapterError]
+          : null;
+        break;
+      default:
+        message = `Prisma Error: ${err.code}`;
+        errors = err.meta ? [err.meta] : null;
+        break;
+    }
+  }
+
+  // Handle Node.js built-in errors and other generic errors
+  else if (err instanceof Error) {
     message = err.message;
-    errorDetails = {
-      name: err.name,
-      cause: err.cause,
-    };
+    errors = [
+      {
+        name: err.name,
+        cause: err.cause,
+      },
+    ];
   }
 
   // Handle PrismaClientValidationError
-  // Handle PrismaClientKnownRequestError
   // Handle PrismaClientUnknownRequestError
   // Handle PrismaClientRustPanicError
   // Handle PrismaClientInitializationError
 
-  res.status(statusCode).json({ success, message, errorDetails });
+  res.status(statusCode).json({ success, message, errors });
 }
 
 export default errorHandler;
