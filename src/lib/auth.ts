344ddab480a013fe02914bd7a497f3e75375e057
md-zeon <zeon.cse@gmail.com>
2026-05-25 17:36:52 +0600

    Updated auth configuration to set baseURL and add Google social provider settings

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,21 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-// If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   trustedOrigins: [process.env.APP_URL || "http://localhost:3000"],
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:4000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000",
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      accessType: "offline",
+      prompt: "select_account consent",
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
